f9b7fff5705d4579c8e1489f9d58c7999675d7d0
AleksEvaAleks <166129085+AleksEvaAleks@users.noreply.github.com>
2024-05-31 12:24:19 +0300

    Delete dist directory

--- dist/index.242b51c6.js
+++
@@ -1,5 +0,0 @@
-const s = "<h1>\u041F\u0440\u0438\u0432\u0435\u0442 \u043F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u0442\u0435\u043B\u044C </h1>";
-const el = document.body;
-el.insertAdjacentHTML("afterbegin", s);
-
-//# sourceMappingURL=index.242b51c6.js.map
